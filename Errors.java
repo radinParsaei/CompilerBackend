86aaebd6e4f76847d9a03da58cdebfeb683ca92e
radinParsaei <radinparsaei@gmail.com>
2021-01-26 15:24:41 +0330

    makes error messages better

--- Errors.java
+++ Errors.java
@@ -1,20 +1,40 @@
 public class Errors {
+  private static String getFunctionName(String functionName) {
+    String[] splitFunctionName = functionName.split(":");
+    String name = splitFunctionName[0];
+    StringBuilder res = new StringBuilder(name).append("(");
+    int parameters = 0;
+    if (splitFunctionName.length > 1) {
+      boolean isFirst = true;
+      for (String string : splitFunctionName[1].split(",")) {
+        if (isFirst) {
+          isFirst = false;
+          continue;
+        }
+        parameters++;
+        res.append(string).append(",");
+      }
+      res.replace(res.length() - 1, res.length(), "");
+    }
+    res.append(")").append(" (").append(name).append(" with ").append(parameters).append(" parameters)");
+    return res.toString();
+  }
   static void error(byte errorCode, String... args) {
     switch (errorCode) {
       case ErrorCodes.ERROR_VARIABLE_DOES_NOT_EXISTS:
         System.err.println("Variable " + args[0] + " does not exists");
         break;
       case ErrorCodes.ERROR_FUNCTION_DOES_NOT_EXISTS:
-        System.err.println("Function " + args[0] + " does not exists");
+        System.err.println("Function " + args[0].split(":")[0] + " does not exists");
         break;
       case ErrorCodes.ERROR_FUNCTION_REDECLARATION:
-        System.err.println("Function " + args[0] + " already exists");
+        System.err.println("Function " + getFunctionName(args[0]) + " already exists");
         break;
       case ErrorCodes.ERROR_ARGS_NOT_MATCH:
-        System.err.println("arguments not match for " + args[0]);
+        System.err.println("arguments not match for " + args[0].split(":")[0]);
         break;
       case ErrorCodes.ERROR_VARIABLE_REDECLARATION:
-        System.err.println("Variable " + args[0] + " already exists");
+        System.err.println("Variable " + args[0].split(":")[0] + " already exists");
         break;
       case ErrorCodes.ERROR_VARIABLE_NOT_DECLARED:
         System.err.println("use of undeclared variable " + args[0]);
@@ -25,6 +45,6 @@ public class Errors {
         else System.out.println();
         break;
     }
-    System.exit(1);
+    if (!Targets.isWeb) System.exit(1);
   }
 }
