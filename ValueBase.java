c07c7a30e6936de4e6f44a93d862951f1cc1c555
radinParsaei <radinparsaei@gmail.com>
2021-04-28 18:08:19 +0430

    ValueBase.equals added

--- ValueBase.java
+++ ValueBase.java
@@ -1,6 +1,28 @@
+import java.math.BigDecimal;
+
 public class ValueBase implements java.io.Serializable {
   private Object data;
   private Data configData = SyntaxTree.getData();
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof SyntaxTree.Number || obj instanceof SyntaxTree.Text || obj instanceof SyntaxTree.Boolean || obj instanceof SyntaxTree.Null || obj instanceof SyntaxTree.List || obj instanceof SyntaxTree.CreateInstance)) {
+      obj = ((ValueBase) obj).getData();
+    }
+    while (obj instanceof ValueBase) {
+      obj = ((ValueBase) obj).getData();
+    }
+    Object value = getData();
+    while (value instanceof ValueBase) {
+      value = ((ValueBase) value).getData();
+    }
+    if (value == null && obj == null) {
+      return true;
+    } else if (value == null || obj == null) {
+      return false;
+    }
+    return value.equals(obj);
+  }
 
   public Object getData() {
     return data;
